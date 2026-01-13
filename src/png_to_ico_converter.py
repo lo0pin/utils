466828739adec64d656fe630425c82be929dc3ff
Julian <136756500+lo0pin@users.noreply.github.com>
2026-01-13 20:25:46 +0100

    Create png_to_ico_converter.py

---
+++ src/png_to_ico_converter.py
@@ -0,0 +1,15 @@
+from PIL import Image
+
+img = Image.open("icon.png").convert("RGBA")
+
+img.save(
+    "icon.ico",
+    sizes=[
+        (16, 16),
+        (32, 32),
+        (48, 48),
+        (64, 64),
+        (128, 128),
+        (256, 256),
+    ]
+)
